65497feb4afd437eae30b46fa0aab4d971636d89
ArthurWuTW <lion811004@gmail.com>
2021-08-17 21:41:09 +0800

    run python

--- youtube_caption.py
+++ youtube_caption.py
@@ -1,5 +1,22 @@
 # pip3 install youtube-transcript-api
+# pip3 install flair
+# docker: docker-script-opensfm
 
 from youtube_transcript_api import YouTubeTranscriptApi
-srt = YouTubeTranscriptApi.get_transcript("SW14tOda_kI")
+srt = YouTubeTranscriptApi.get_transcript("G-h_kEG_Xnk")
+
+for sentence in srt:
+    print(sentence)
+
+sentence_arr = [sent['text'] for sent in srt]
+
+sentence = ' '.join(sentence_arr)
+
+import flair
+sentiment_model = flair.models.TextClassifier.load('en-sentiment')
+sentence = flair.data.Sentence(sentence)
+sentiment_model.predict(sentence)
+print(sentence)
+
+
 
