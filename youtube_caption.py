3b354d2a49d6d0aa4ad095b1a1b7737ec9f96bc5
ArthurWuTW <lion811004@gmail.com>
2021-08-18 21:07:43 +0800

    search+sentiment

--- youtube_caption.py
+++ youtube_caption.py
@@ -8,24 +8,53 @@
 # flair.cache_root = Path("/your/path/.flair")# WORKS
 
 
+# from youtube_transcript_api import YouTubeTranscriptApi
+# srt = YouTubeTranscriptApi.get_transcript("zS2DFYKzVI0")
+
+# for sentence in srt:
+#     print(sentence)
+
+# sentence_arr = [sent['text'] for sent in srt]
+
+# sentence = ' '.join(sentence_arr)
+
+# import flair
+# from pathlib import Path
+# # import flair
+# flair.cache_root = Path("/home/user/flair_cache")# WORKS
+# sentiment_model = flair.models.TextClassifier.load('en-sentiment')
+# sentence = flair.data.Sentence(sentence)
+# sentiment_model.predict(sentence)
+# print(sentence)
+
 from youtube_transcript_api import YouTubeTranscriptApi
-srt = YouTubeTranscriptApi.get_transcript("zS2DFYKzVI0")
-
-for sentence in srt:
-    print(sentence)
-
-sentence_arr = [sent['text'] for sent in srt]
-
-sentence = ' '.join(sentence_arr)
-
+from youtubesearchpython import *
+import time
 import flair
 from pathlib import Path
-# import flair
 flair.cache_root = Path("/home/user/flair_cache")# WORKS
 sentiment_model = flair.models.TextClassifier.load('en-sentiment')
-sentence = flair.data.Sentence(sentence)
-sentiment_model.predict(sentence)
-print(sentence)
+customSearch = CustomSearch('tesla', VideoSortOrder.uploadDate, limit = 20)
+
+print(customSearch.result())
+
+
+
+for result in customSearch.result()['result']:
+    try:
+        print("------------------")
+        print(result['id'])
+        print(result['title'])
+        srt = YouTubeTranscriptApi.get_transcript(result['id'])
+        sentence_arr = [sent['text'] for sent in srt]
+        sentence = ' '.join(sentence_arr)
+        # print(sentence)
+        sentence = flair.data.Sentence(sentence)
+        sentiment_model.predict(sentence)
+        print(sentence.labels)
+        time.sleep(10)
+    except:
+        print("cannot get subtitles")
 
 
 
